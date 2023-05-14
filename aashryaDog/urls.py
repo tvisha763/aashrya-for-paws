b7a687171a62d97f3d51b00b8c987c1b2a71f3ba
Tvisha Choubey <tc9083@pleasantonusd.net>
2023-05-14 16:16:41 -0700

    fourth

--- aashryaDog/urls.py
+++ aashryaDog/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
-    path(r'/', include('project.urls')),
+    path(r'', include('project.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
