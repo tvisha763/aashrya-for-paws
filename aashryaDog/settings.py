08afbd71bcc1c3d6d082b330bb52e81c9bbfeb39
Tvisha Choubey <tc9083@pleasantonusd.net>
2023-05-14 16:25:25 -0700

    fourth

--- aashryaDog/settings.py
+++ aashryaDog/settings.py
@@ -129,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["https://aashrya_for-paws.herokuapp.com"]
