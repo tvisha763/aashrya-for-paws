8181e08ce95b57e6dc0c1ac0c19ca6aa60cba26f
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2023-10-06 20:28:09 -0700

    Update settings.py

--- aashryaDog/settings.py
+++ aashryaDog/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["https://aashrya-for-paws.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://aashryaforpaws-d00c2266571b.herokuapp.com/", "127.0.0.1"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
